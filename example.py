f5478f4dfd24b3620a55baec7972f624926a5319
doggonator <chandonaaron@rocketmail.com>
2020-07-29 15:47:14 +0100

    Hellow Werwd

--- example.py
+++ example.py
@@ -6,7 +6,19 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
 
-win = Gtk.Window()
+class MyWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Hello World")
+
+        self.button = Gtk.Button(label="Click Here")
+        self.button.connect("clicked", self.on_button_clicked)
+        self.add(self.button)
+
+    def on_button_clicked(self, widget):
+        print("Hewwo Werwd OwO")
+
+
+win = MyWindow()
 win.connect("destroy", Gtk.main_quit)
 win.show_all()
 Gtk.main()
